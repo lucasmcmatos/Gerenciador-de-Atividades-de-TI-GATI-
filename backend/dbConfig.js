cf4e3f0f3be71d5e737db69666010f89279eeda6
Lucas Matos <lucasmmatos05@gmail.com>
2024-03-06 01:11:48 -0300

    Data base connection

---
+++ backend/dbConfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+    user:'postgres',
+    host:'localhost',
+    database:'GATI - Legolar',
+    password: '272930Lkw',
+    port: 5432
+}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const {Pool} = require('pg');
+const dbConfig = require('./dbConfig');
+
+// Configuracoes de banco de dados
+
+const pool = new Pool(dbConfig);
+
+pool.connect().then(()=>{
+    console.log('Conexão com o banco de dados foi BEM sucedida.')
+}).catch(err => {
+    console.log('Houve um erro na conexão com banco de dados:', err)
+})
+
+// Configuracoes de servidor
 
 app.get('/' , (req,res)=>{
     res.send('Gerenciador de Atividades de TI')
